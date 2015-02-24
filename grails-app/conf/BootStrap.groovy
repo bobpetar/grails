b0556d34dd3b6cd169f094c5d56b0b60d3790cc0
Balkrishna Pandey <sachit.nep@gmail.com>
2015-02-24 12:08:33 +0545

    Default admin role created

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,18 @@
+import com.retouch.Role
+import com.retouch.User
+import com.retouch.UserRole;
+
 class BootStrap {
 
     def init = { servletContext ->
+		def checkuser = Role.findByAuthority('ROLE_ADMIN')
+		if(!checkuser){
+			def adminRole = new	Role(authority:'ROLE_ADMIN')
+			adminRole.save(flush:true)
+			def adminUser = new User(username: 'admin', password: 'admin')
+			adminUser.save(flush:true)		
+			UserRole.create adminUser, adminRole, true
+		}		
     }
     def destroy = {
     }
