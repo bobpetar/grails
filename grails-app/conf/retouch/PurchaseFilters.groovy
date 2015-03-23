9dd43b781770c8505f41aed5eeaeab2b3c131293
Balkrishna Pandey <sachit.nep@gmail.com>
2015-03-23 16:09:31 +0545

    Paypal base url update, purchase completed filter no longer necessary

--- grails-app/conf/retouch/PurchaseFilters.groovy
+++ grails-app/conf/retouch/PurchaseFilters.groovy
@@ -7,12 +7,10 @@ class PurchaseFilters {
     def filters = {
         all(controller: 'paypal', action: 'buy') {
             before = {
-                params.baseUrl ="http://localhost:8080/retouch"
+                params.baseUrl = grailsApplication.config.grails.serverURL
             }
             after = { Map model ->
-                println("Payment Instance " + request.payment)
                 def payment = request.payment
-                println("Payment transaction "+params)
                 def projectInstance = Project.findByProjectId(params.itemName)
                 projectInstance.task.payment=payment
                 projectInstance.save(flush: true)
