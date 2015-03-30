5da69aa7218a2caee102bedb605b8a32d5c4ea03
aasizvaidya <aasizvaidya@gmail.com>
2015-03-30 13:56:08 +0545

    Coupon generation

--- grails-app/conf/retouch/PurchaseFilters.groovy
+++ grails-app/conf/retouch/PurchaseFilters.groovy
@@ -1,6 +1,7 @@
 package retouch
 
 import com.retouch.Task
+import com.retouch.User
 import org.grails.paypal.Payment
 
 class PurchaseFilters {
@@ -33,6 +34,7 @@ class PurchaseFilters {
                 def payment = request.payment
                 if(payment && payment.status == Payment.COMPLETE) {
                     def task = Task.findByPayment(payment)
+                    taskService.triggerConfirmation(task.project.client)
                     task.project.status = "Paid"
                     task.project.save()
                     taskService.notifyRetouchers(task)
