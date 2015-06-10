1f5bf2235e59f8954b73f39af8d1b60f370613d6
Balkrishna Pandey <sachit.nep@gmail.com>
2015-06-10 13:04:55 +0545

    qa environment added

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -68,4 +68,38 @@ environments {
             }
         }
     }
+
+    qa {
+        dataSource {
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:mysql://localhost:3306/retouch?zeroDateTimeBehavior=convertToNull"
+            username = "retouchadmin"
+            password = "^Gdh&1234"
+            pooled = true
+            jmxExport = true
+            driverClassName = "com.mysql.jdbc.Driver"
+            dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
+            logSql = false
+            properties {
+                // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
+                jmxEnabled = true
+                initialSize = 5
+                maxActive = 50
+                minIdle = 5
+                maxIdle = 25
+                maxWait = 10000
+                maxAge = 10 * 60000
+                timeBetweenEvictionRunsMillis = 5000
+                minEvictableIdleTimeMillis = 60000
+                validationQuery = "SELECT 1"
+                validationQueryTimeout = 3
+                validationInterval = 15000
+                testOnBorrow = true
+                testWhileIdle = true
+                testOnReturn = false
+                jdbcInterceptors = "ConnectionState"
+                defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
+            }
+        }
+    }
 }
